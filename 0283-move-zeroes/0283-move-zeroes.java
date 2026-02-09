1c3b2ebcf44441840095d1113b5c5d37d1758afb
vtu25635 <vtu25635@veltech.edu.in>
2026-02-09 09:34:26 +0530

    Time: 3 ms (19.35%), Space: 47.6 MB (92.38%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = nums[i];
+                nums[i] = temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+    }
+}
