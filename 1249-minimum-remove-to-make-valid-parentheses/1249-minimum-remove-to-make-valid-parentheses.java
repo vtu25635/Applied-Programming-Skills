d794bfa4a3eef44e46ec880cca3967db949b51e9
vtu25635 <vtu25635@veltech.edu.in>
2026-03-03 10:29:48 +0530

    Time: 21 ms (49.03%), Space: 47.4 MB (51.42%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    sb.setCharAt(i, '*');
+                }
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.setCharAt(stack.pop(), '*');
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) != '*') {
+                result.append(sb.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
