b3190ffdb877b06777bd9bc0622b89e1fa777482
vtu25635 <vtu25635@veltech.edu.in>
2026-05-01 10:52:17 +0530

    Time: 2 ms (90.28%) | Memory: 44.4 MB (25.50%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCount = 0;
+
+        // 1. Initial Scan
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 2) {
+                    queue.offer(new int[]{r, c});
+                } else if (grid[r][c] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0) return 0;
+
+        int minutes = 0;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        // 2. BFS Multidirectional Spread
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean rottedThisMinute = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                for (int[] dir : directions) {
+                    int nr = curr[0] + dir[0];
+                    int nc = curr[1] + dir[1];
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2; // Infect the orange
+                        queue.offer(new int[]{nr, nc});
+                        freshCount--;
+                        rottedThisMinute = true;
+                    }
+                }
+            }
+            if (rottedThisMinute) minutes++;
+        }
+
+        return freshCount == 0 ? minutes : -1;
+    }
+}
