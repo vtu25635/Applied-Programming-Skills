0f97beaa0fc7f690caa6c4f1be9db56ff06bc445
vtu25635 <vtu25635@veltech.edu.in>
2026-05-01 10:46:01 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (67.22%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                // If we find land, calculate the area of that island
+                if (grid[r][c] == 1) {
+                    maxArea = Math.max(maxArea, dfs(grid, r, c));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int r, int c) {
+        // Base case: Out of bounds or water
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
+            return 0;
+        }
+
+        // Mark as visited by sinking the island
+        grid[r][c] = 0;
+
+        // Count this cell + neighbors
+        return 1 + dfs(grid, r + 1, c) 
+                 + dfs(grid, r - 1, c) 
+                 + dfs(grid, r, c + 1) 
+                 + dfs(grid, r, c - 1);
+    }
+}
